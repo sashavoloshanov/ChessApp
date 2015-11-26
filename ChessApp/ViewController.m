6fc6ddf99c270fe121ca655e97cf56f5b6b07f90
sashavoloshanov <svoloshanov@icloud.com>
2015-11-26 20:55:36 +0200

    test2

--- ChessApp/ViewController.m
+++ ChessApp/ViewController.m
@@ -10,47 +10,97 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) NSMutableArray *cellsArray;
+@property (assign, nonatomic) NSInteger viewWidth;
+@property (strong, nonatomic) UIView *staticChess;
+@property (strong, nonatomic) UIView *currentChess;
+
 @end
 
 @implementation ViewController
 
+int count = 0;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    float viewHeight = self.view.frame.size.height / 8;
-    float viewWidth = self.view.frame.size.width / 8;
+    CGFloat viewWidth = self.view.frame.size.width / 8;
+    _viewWidth = viewWidth;
     
-    for (int index1 = 0; index1 < 8; index1++) {
-        for (int index2 = 0; index2 < 8; index2++) {
-            UIView *chessView = [[UIView alloc] initWithFrame:CGRectMake(index1 * viewWidth, index2 * viewWidth, viewHeight, viewWidth)];
-            if (index2 % 2 == 0) {
-                if (index1 % 2 == 0) {
-                chessView.backgroundColor = [UIColor blackColor];
-                } else {
-                chessView.backgroundColor = [UIColor greenColor];
+    self.cellsArray = [NSMutableArray array];
+    
+    for (int indexX = 0; indexX < 8; indexX++) {
+        for (int indexY = 1; indexY <= 8; indexY++) {
+            
+            if (indexY % 2 == 0) {
+                if (indexX % 2 == 0) {
+                    UIView *cellView = [[UIView alloc] initWithFrame:CGRectMake(indexX * viewWidth,
+                                                                                indexY * viewWidth,
+                                                                                viewWidth, viewWidth)];
+                    cellView.backgroundColor = [UIColor blackColor];
+                    [self.view addSubview:cellView];
+                    [self.cellsArray addObject:cellView];
+                    
                 }
             } else {
-                if (index1 % 2 != 0) {
-                    chessView.backgroundColor = [UIColor blackColor];
-                } else {
-                    chessView.backgroundColor = [UIColor greenColor];
+                if (indexX % 2 != 0) {
+                    UIView *cellView = [[UIView alloc] initWithFrame:CGRectMake(indexX * viewWidth,
+                                                                                indexY * viewWidth,
+                                                                                viewWidth, viewWidth)];
+                    cellView.backgroundColor = [UIColor blackColor];
+                    [self.view addSubview:cellView];
+                    [self.cellsArray addObject:cellView];
+                    
                 }
             }
             
-            chessView.autoresizingMask = UIViewContentModeTop;
-            
-            [self.view addSubview:chessView];
-            
-        
+            }
         }
-    }
     
+    self.staticChess = [[UIView alloc] initWithFrame:CGRectMake(CGRectGetMidX(self.view.frame) - _viewWidth / 2,
+                                                             CGRectGetHeight(self.view.frame) - _viewWidth,
+                                                             _viewWidth - 20,
+                                                             _viewWidth - 20)];
+    self.staticChess.layer.cornerRadius = CGRectGetWidth(self.staticChess.frame) / 2;
+    self.staticChess.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:self.staticChess];
+}
+
+- (void)setChessToCell {
+    self.currentChess = [[UIView alloc] initWithFrame:self.staticChess.frame];
+    self.currentChess.layer.cornerRadius = CGRectGetWidth(self.currentChess.frame) / 2;
+    count++;
+    [self.view addSubview:self.currentChess];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Touch Handling
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    for (UITouch *touch in touches) {
+        CGPoint location = [touch locationInView:self.view];
+        
+        [self setChessToCell];
+        
+        for (UIView *tappedCell in self.cellsArray) {
+            
+            if (CGRectContainsPoint(tappedCell.frame, location)) {
+                
+                [UIView animateWithDuration:1.0 animations:^{
+                    self.currentChess.center = tappedCell.center;
+                }];
+                
+                }
+            }
+        }
+//                if (count % 2 != 0) {
+//                    self.currentChess.backgroundColor = self.staticChess.backgroundColor = [UIColor blueColor];
+//                    
+//                } else {
+//                    self.currentChess.backgroundColor = self.staticChess.backgroundColor = [UIColor greenColor];
+//                }
 }
+
 
 @end
